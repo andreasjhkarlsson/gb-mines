216efa2b16b189c22d504878054c5e00120918e4
Andreas Karlsson <andreas.karlsson3@gmail.com>
2016-02-19 21:59:31 +0100

    Add startup screen

--- frontends/gameboy/main.c
+++ frontends/gameboy/main.c
@@ -1,7 +1,21 @@
 #include <gb/gb.h>
+#include <gb/drawing.h>
+#include <asm/types.h>
 #include <board.h>
+#include "logo.h"
+
+
+void show_logo()
+{
+	set_bkg_data(0, logo_tile_count, logo_tile_data);
+	set_bkg_tiles(0, 0, logo_tile_map_width, logo_tile_map_height, logo_map_data);
+	SHOW_BKG;
+	DISPLAY_ON;
+}
 
 void main()
 {
+	show_logo();
+
 
 }
